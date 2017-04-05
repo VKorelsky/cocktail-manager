e729b64f33660b0e1b7e447cc3b74d1d57e1c73b
Victor Korelsky <victor.korelsky@mail.mcgill.ca>
2017-04-05 19:48:56 -0400

    controller

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,2 +1,32 @@
 class CocktailsController < ApplicationController
+
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def show
+    raise
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
+  end
 end
--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,2 +1,16 @@
 class DosesController < ApplicationController
+
+  def create
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description, :cocktail_id, :ingredient_id)
+  end
 end
